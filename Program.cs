e18ae0e4e52f7d41fab2d8b492db2da4ccba6086
Koiあきひろ <100710406+KID-FURY0@users.noreply.github.com>
2022-11-02 13:47:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDetails
+{
+    class StudentDetails
+    {
+        public string naa;
+        private int idd;
+        private string StudentAddress;
+
+        public string getAddress()
+        {
+            return StudentAddress;
+        }
+        public void setAddress(string SAddreess)
+        {
+            StudentAddress = SAddreess;
+            Console.WriteLine("Address: " + StudentAddress);
+        }
+
+        public void getStudentName(string name)
+        {
+            naa = name;
+            Console.WriteLine("Name: " + name);
+        }
+        public void getStudentId(int id)
+        {
+            idd = id;
+            Console.WriteLine("ID: " + id);
+        }
+        public void store()
+        {
+            Dictionary<int, string> dc = new Dictionary<int, string>();
+            dc.Add(1, naa);
+            dc.Add(3, StudentAddress);
+            dc.Add(2, idd.ToString());
+
+            foreach (KeyValuePair<int, string> i in dc)
+            {
+                Console.WriteLine(i.Value);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            StudentDetails std = new StudentDetails();
+            std.getStudentName("Sue");
+            std.getStudentId(456);
+            std.setAddress("Mbale");
+
+            Console.WriteLine("\nDetails stored in the Dictioary!\n");
+            std.store();
+        }
+    }
+
+}
+
